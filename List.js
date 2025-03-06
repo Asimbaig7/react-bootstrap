cf809be53a4e2fd089a1fc69424dd1d35d403ac3
Asim Baig <asimmanshadbaig@gmail.com>
2025-03-07 00:03:33 +0500

    List.js

---
+++ List.js
@@ -0,0 +1,18 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function DefaultExample() {
+  return (
+  
+    <ListGroup>
+      <ListGroup.Item  style={{backgroundColor:'black',color:'white',width:'300px',marginLeft:'20px'}}>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item   style={{backgroundColor:'black',color:'white',width:'300px',marginLeft:'20px'}}>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item   style={{backgroundColor:'black',color:'white',width:'300px',marginLeft:'20px'}}>Morbi leo risus</ListGroup.Item>
+      <ListGroup.Item   style={{backgroundColor:'black',color:'white',width:'300px',marginLeft:'20px'}}>Porta ac consectetur ac</ListGroup.Item>
+      <ListGroup.Item   style={{backgroundColor:'black',color:'white',width:'300px',marginLeft:'20px'}}>Vestibulum at eros</ListGroup.Item>
+    </ListGroup>
+
+  );
+}
+
+
+export default DefaultExample;
